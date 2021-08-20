5de356f3435ae54f8b67085dc5f6b16d9c7e2777
Tanuj1789 <58630657+Tanuj1789@users.noreply.github.com>
2021-08-20 22:28:27 +0530

    first commit

---
+++ mart/views.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+from django.shortcuts import render,HttpResponse
+from mart.models import Contact
+from datetime import datetime
+from django.contrib import messages
+
+# Create your views here.
+def index(request):
+    # context dictionary h
+    context ={
+        'variable': 'this is sent'
+    }
+    return render(request,'index.html', context)
+
+def about(request):
+    context ={
+        'variable': 'this is sent'
+    }
+    return render(request,'about.html', context)
+
+def service(request):
+    context ={
+        'variable': 'this is sent'
+    }
+    return render(request,'service.html', context)
+
+def contact(request):
+    if request.method == "POST":
+        fname= request.POST.get('fname')
+        lname= request.POST.get('lname')
+        email= request.POST.get('email')
+        desc= request.POST.get('desc')
+        contact= Contact(fname=fname, lname=lname, email=email, desc= desc, date=datetime.today())
+        contact.save()
+        messages.success(request, 'Thank you for the feedback!')
+    return render(request,'contact.html')
